a74c0c8fc3ac09a0a4aed07b3a6e541552b36897
Mohammad Javad Alizadeh <mrph14@gmail.com>
2024-01-01 11:51:21 +0330

    add checkpoints

--- src/inference.py
+++ src/inference.py
@@ -1,8 +1,10 @@
 from transformers import pipeline
 
-checkpoint = "facebook/mbart-large-50-many-to-many-mmt"
-# checkpoint = "/home/mrph/Desktop/LEARNit/facebookMBart50/facebook/mbart-large-50-many-to-many-mmt_finetuned/checkpoint-3"
+# checkpoint = "facebook/mbart-large-50-many-to-many-mmt"
+# checkpoint = "/home/mrph/Desktop/LEARNit/facebookMBart50/saved_models/checkpoint-57465"
+# checkpoint = "/home/mrph/Desktop/LEARNit/facebookMBart50/saved_models/checkpoint-114930"
+checkpoint = "/home/mrph/Desktop/LEARNit/facebookMBart50/saved_models/checkpoint-229860"
 translator = pipeline("translation",
                       model=checkpoint,
                       src_lang="en_XX", tgt_lang="fa_IR")
-print(translator("Hello World!"))
+print(translator("trying not to laugh, but I didn't understand"))
