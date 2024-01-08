f910731149d7bc51c57fc1dbc90e85536cb697df
Mohammad Javad Alizadeh <mrph14@gmail.com>
2024-01-08 10:12:14 +0330

    fix rtl

--- src/streamlit_demo.py
+++ src/streamlit_demo.py
@@ -4,7 +4,7 @@ import streamlit as st
 from inference import Inference
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
-saved_model_path = os.path.join(dir_path, '..', 'saved_model', 'facebook_finetuned')
+saved_model_path = os.path.join(dir_path, '..', 'saved_model')
 
 model_checkpoint_paths = sorted([os.path.join(saved_model_path, path) for path in os.listdir(saved_model_path)],
                                 key=os.path.getmtime)
@@ -26,7 +26,8 @@ def set_model():
 
 
 st.title('Machine Translation mBart50')
-option = st.selectbox("Model checkpoint", model_checkpoint_paths, index=None, placeholder="Select a checkpoint",
+option = st.selectbox("Model checkpoint", model_checkpoint_paths, index=None,
+                      placeholder="Select a checkpoint (the last one is the newest checkpoint)",
                       on_change=set_model, key='checkpoint')
 
 src_text = st.text_area(label="Sentence", height=200)
@@ -36,4 +37,7 @@ btn_disabled = True if src_text == "" or option is None else False
 if st.button(label="Translate", disabled=btn_disabled):
     with st.spinner(""):
         translation_text = st.session_state.model.translate(src_text)
-    st.success(translation_text)
+    # st.success(translation_text)
+    st.markdown(
+        f"<p dir='rtl' style='background-color:rgba(61, 213, 109, 0.2);border-radius:0.5rem;padding:16px;line-height:1.6;'>{translation_text}</p>",
+        unsafe_allow_html=True)
