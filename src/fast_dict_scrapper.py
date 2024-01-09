49a69304eb74e6c5199bdc909aece18faaa90f4f
Mohammad Javad Alizadeh <mrph14@gmail.com>
2024-01-09 09:56:51 +0330

    change word data

--- src/fast_dict_scrapper.py
+++ src/fast_dict_scrapper.py
@@ -1,20 +1,21 @@
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
+import time
 
-with open('./../data/words_alpha.txt') as file:
+with open('./../data/wordlist.txt') as file:
     words = file.read()
 
 words = words.split('\n')
 examples = []
 
-for word in words:
-    link = 'https://fastdic.com/word/{}'.format(word)
-    soup = BeautifulSoup(requests.get(link).content)
-    for i in soup.find_all(class_='result__sentences'):
-        print(i.text.strip().split('\n'))
-        examples.append(i.text.strip().split('\n'))
-
-df = pd.DataFrame(examples)
-
-df.to_csv('fastdict_examples.csv')
+try:
+    for word in words:
+        link = 'https://fastdic.com/word/{}'.format(word)
+        soup = BeautifulSoup(requests.get(link).content)
+        for i in soup.find_all(class_='result__sentences'):
+            print(i.text.strip().split('\n'))
+            examples.append(i.text.strip().split('\n'))
+finally:
+    df = pd.DataFrame(examples)
+    df.to_csv(f'fastdict_examples_{time.time()}.csv')
